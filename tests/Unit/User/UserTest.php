14e6b65703d18289702d21fe5d02c80f869833b6
Oaparecido <daniel.aparecido4683@gmail.com>
2021-03-03 15:06:18 -0300

    ✅ update tests successfully

---
+++ tests/Unit/User/UserTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Unit\User;
+
+use PHPUnit\Framework\TestCase;
+use Tests\Mock\Load\LoadMock;
+
+class UserTest extends TestCase
+{
+    private $mock;
+
+    protected function setUp(): void
+    {
+        $this->mock = LoadMock::getDataUser();
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     */
+    public function create()
+    {
+        //TODO (feature) -> Create user and save data in DB.
+        $controller = new Controllers\UserController();
+
+        $response = $controller->store($this->mock);
+
+        $expected = [
+            'code' => 201,
+            'success' => true,
+            'params' => [
+                'message' => 'user.created-successfully',
+                'name' => '',
+                'email' => '',
+                'age' => '',
+                'contacts' => '',
+            ],
+        ];
+
+        $this->assertEquals($expected, $response, 'Response not is equal' . PHP_EOL . var_dump('Response:' . PHP_EOL, $response) . var_dump('Expected:' . PHP_EOL, $expected));
+        return $response;
+    }
+
+    /**
+     * @test
+     * @depends create
+     * @param array $data
+     */
+    public function login(array $data)
+    {
+        $data = [
+            'email' => $data['params']['email'],
+            'password' => '@Senha123'
+        ];
+
+        $controller = new Controllers\AuthController();
+        $response = $controller->auth($data);
+
+        $expected = [
+            'code' => 201,
+            'success' => true,
+            'params' => [
+                'authenticated' => true,
+                'message' => 'user.logged-successfully',
+                'logged_data' => [
+                    'id' => '',
+                    'name' => ''
+                ],
+            ],
+        ];
+
+        $this->assertEquals($expected, $response, 'Response not is equal' . PHP_EOL . var_dump('Response:' . PHP_EOL, $response) . var_dump('Expected:' . PHP_EOL, $expected));
+    }
+
+}
